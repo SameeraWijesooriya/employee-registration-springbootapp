adf0685593b7d96c928c6e076199d8826f30beaf
sameera <sameeram.wijesooriya95@gmail.com>
2023-07-13 17:34:28 +0530

    Start create Backend useing springBoot

---
+++ backend/employeems/src/main/java/lk/ijse/dep10/employeems/EmployeemsApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.employeems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemsApplication.class, args);
+	}
+
+}
